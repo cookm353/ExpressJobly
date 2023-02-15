946efe90a93071917f2948dffd406dbe4ec43d5d
Matthew Cook <cookm353@gmail.com>
2023-02-15 14:15:38 -0500

    Adding jobs

--- dist/models/_testCommon.js
+++ dist/models/_testCommon.js
@@ -6,6 +6,7 @@ async function commonBeforeAll() {
     await db.query("DELETE FROM companies");
     // noinspection SqlWithoutWhere
     await db.query("DELETE FROM users");
+    await db.query("DELETE FROM jobs");
     await db.query(`
     INSERT INTO companies(handle, name, num_employees, description, logo_url)
     VALUES ('c1', 'C1', 1, 'Desc1', 'http://c1.img'),
@@ -23,6 +24,13 @@ async function commonBeforeAll() {
         await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
         await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
     ]);
+    await db.query(`
+      INSERT INTO jobs(title, salary, equity, company_handle)
+      VALUES  ('j1', 90000, 0.6, 'c1'),
+              ('j2', 65000, 0.0, 'c2'),
+              ('j3', 100000, 0.9, 'c3')
+      RETURNING title, salary, equity, company_handle
+  `);
 }
 async function commonBeforeEach() {
     await db.query("BEGIN");
