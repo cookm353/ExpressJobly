1d25ea667e42d2dabf2c7c29c10842c3a376a52d
Matthew Cook <cookm353@gmail.com>
2023-02-18 15:29:34 -0500

    Added jobs to response

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -84,8 +84,8 @@ router.get("/", async function (req, res, next) {
 
 router.get("/:handle", async function (req, res, next) {
   try {
-    const company = await Company.get(req.params.handle);
-    return res.json({ company });
+    const [company, jobs] = await Company.get(req.params.handle);
+    return res.json({ company, jobs });
   } catch (err) {
     return next(err);
   }
